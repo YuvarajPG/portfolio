05eef6761efb2b4ec0812afea3b58e6e2c9544a3
YuvarajPG <yuvar2978@gmail.com>
2026-03-31 17:30:27 +0530

    feat: enhance download button animation for improved user experience

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -119,7 +119,7 @@ const Main = () => {
                     <a
                         href="/resume_v2_updated.pdf"
                         download="Yuvaraj_Resume.pdf"
-                        className="py-1.5 rounded-full font-medium transition-all hover:scale-105 z-99 text-white bg-orange-700 px-4 mt-5 animate-in fade-in duration-150 ease-in block text-center hover:cursor-pointer hover:bg-orange-600"
+                        className="py-1.5 rounded-full font-medium transition-all hover:scale-105 z-99 text-white bg-orange-700 px-4 mt-5 animate-in fade-in duration-150 ease-in block text-center hover:cursor-pointer hover:bg-orange-600 animate-in fade-in duration-3000 ease-in"
                     >
                         Download CV
                     </a>
